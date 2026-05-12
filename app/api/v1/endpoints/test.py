efa56b8c4fe080b42530fa3410cd2d2790dace24
Anjul Singh <anjulsingh462@gmail.com>
2026-05-12 18:56:11 +0530

    fix test route issue

--- app/api/v1/endpoints/test.py
+++ app/api/v1/endpoints/test.py
@@ -1,13 +1,8 @@
-from sqlalchemy.orm import Session
 
-from fastapi import APIRouter,Depends, Request,status,Response
-from app.controller import auth
-from app.schemas.auth_schema import ResetPasswordSchema, SignupSchema,LoginSchema,GoogleAuthSchema,OTPSchema,OTPVerifySchema
-from app.core.database import get_db
+from fastapi import APIRouter,status
 
+test_router = APIRouter(prefix="/test",tags=["Test"])
 
-auth_router = APIRouter(prefix="/test",tags=["Test"])
-
-@auth_router.get("/",status_code=status.HTTP_201_CREATED)
+@test_router.get("/",status_code=status.HTTP_201_CREATED)
 async def test_endpoint():
     return {"message": "Test endpoint is working!"}
